f4590d0007f8cc3064f12a350b115ebfe03a61a2
Aconstantinesei Ionut Silviu <iaconstantinesei@stud.acs.upb.ro>
2026-05-20 14:39:20 +0300

    ADD: Am adaugat in functia main rezolvarea pentru task 4.

--- src/interviu.c
+++ src/interviu.c
@@ -20,6 +20,152 @@ int main(int argc, char const *argv[])
 
     if (isdigit(buffer[0]))
     {
+        FILE *check = fopen(argv[1], "r");
+        char t1[100], t2[100], t3[100];
+        fscanf(check, "%99s %99s %99s", t1, t2, t3);
+        fclose(check);
+
+        int is_task4 = (strchr(t3, '.') == NULL);
+
+        FILE *fin_cif = fopen(argv[1], "r");
+        int n_cif;
+        fscanf(fin_cif, "%d", &n_cif);
+
+        if (is_task4) 
+        {
+            double delta, pret_start, pret_tinta;
+            int k_zile;
+            fscanf(fin_cif, "%lf", &delta);
+            fscanf(fin_cif, "%d", &k_zile);
+            fscanf(fin_cif, "%lf", &pret_start);
+            fscanf(fin_cif, "%lf", &pret_tinta);
+
+            double *preturi = (double *)malloc(n_cif * sizeof(double));
+            for (int i = 0; i < n_cif; i++)
+            {
+                fscanf(fin_cif, "%lf", &preturi[i]);
+            }
+
+            int stare_minima = (int)floor(preturi[0] / delta);
+            int stare_maxima = stare_minima;
+
+            int stare_start = (int)floor(pret_start / delta);
+            int stare_tinta = (int)floor(pret_tinta / delta);
+
+            if (stare_start < stare_minima)
+                stare_minima = stare_start;
+            if (stare_tinta < stare_minima)
+                stare_minima = stare_tinta;
+            if (stare_start > stare_maxima)
+                stare_maxima = stare_start;
+            if (stare_tinta > stare_maxima)
+                stare_maxima = stare_tinta;
+
+            for (int i = 0; i < n_cif; i++)
+            {
+                int stare = (int)floor(preturi[i] / delta);
+                if (stare < stare_minima)
+                    stare_minima = stare;
+                if (stare > stare_maxima)
+                    stare_maxima = stare;
+            }
+
+            int nr_stari = stare_maxima - stare_minima + 1;
+
+            int **tranzitii = (int **)calloc(nr_stari, sizeof(int *));
+            for (int i = 0; i < nr_stari; i++)
+            {
+                tranzitii[i] = (int *)calloc(nr_stari, sizeof(int));
+            }
+            int *grad_iesire = (int *)calloc(nr_stari, sizeof(int));
+
+            for (int i = 0; i < n_cif - 1; i++)
+            {
+                int u = (int)floor(preturi[i] / delta) - stare_minima;
+                int v = (int)floor(preturi[i + 1] / delta) - stare_minima;
+                tranzitii[u][v]++;
+                grad_iesire[u]++;
+            }
+
+            Fractie *prob_curente = (Fractie *)malloc(nr_stari * sizeof(Fractie));
+            Fractie *prob_urmatoare = (Fractie *)malloc(nr_stari * sizeof(Fractie));
+
+            for (int i = 0; i < nr_stari; i++)
+            {
+                prob_curente[i] = (Fractie){0, 1};
+            }
+
+            int index_start = stare_start - stare_minima;
+            int index_tinta = stare_tinta - stare_minima;
+            prob_curente[index_start] = (Fractie){1, 1};
+
+            FILE *fout = fopen(argv[2], "w");
+
+            for (int zi = 1; zi <= k_zile; zi++)
+            {
+                if (prob_curente[index_tinta].numarator == 0)
+                {
+                    fprintf(fout, "0");
+                }
+                else if (prob_curente[index_tinta].numarator == prob_curente[index_tinta].numitor)
+                {
+                    fprintf(fout, "1");
+                }
+                else
+                {
+                    fprintf(fout, "%lld/%lld", prob_curente[index_tinta].numarator, prob_curente[index_tinta].numitor);
+                }
+                if (zi < k_zile)
+                {
+                    fprintf(fout, "\n");
+                }
+
+                for (int i = 0; i < nr_stari; i++)
+                {
+                    prob_urmatoare[i] = (Fractie){0, 1};
+                }
+
+                for (int i = 0; i < nr_stari; i++)
+                {
+                    if (prob_curente[i].numarator == 0)
+                        continue;
+
+                    if (grad_iesire[i] == 0)
+                    {
+                        prob_urmatoare[i] = aduna(prob_urmatoare[i], prob_curente[i]);
+                    }
+                    else
+                    {
+                        for (int j = 0; j < nr_stari; j++)
+                        {
+                            if (tranzitii[i][j] > 0)
+                            {
+                                Fractie prob_tranzitie = {(long long)tranzitii[i][j], (long long)grad_iesire[i]};
+                                Fractie prob_mutata = inmulteste(prob_curente[i], prob_tranzitie);
+                                prob_urmatoare[j] = aduna(prob_urmatoare[j], prob_mutata);
+                            }
+                        }
+                    }
+                }
+
+                for (int i = 0; i < nr_stari; i++)
+                {
+                    prob_curente[i] = prob_urmatoare[i];
+                }
+            }
+
+            for (int i = 0; i < nr_stari; i++)
+                free(tranzitii[i]);
+            free(tranzitii);
+            free(grad_iesire);
+            free(prob_curente);
+            free(prob_urmatoare);
+            free(preturi);
+            fclose(fin_cif);
+            fclose(fout);
+        }
+        else
+        {
             FILE *fin = fopen(argv[1], "r");
             int n;
             fscanf(fin, "%d", &n);
@@ -46,7 +192,7 @@ int main(int argc, char const *argv[])
             fprintf(fout, "%.3f\n", sharpe);
             freeList(&head);
             fclose(fout);
-        
+        }
     
     }
     else
